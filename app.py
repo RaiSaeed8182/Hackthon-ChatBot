dd3ffaafbb60a85631e40cd7c8c21abdb3e9f534
RaiSaeed8182 <saeedrai81829@gmail.com>
2025-11-24 22:48:25 +0500

    Fix langchain imports for cloud compatibility

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ from scipy.io import wavfile
 
 # Audio recording not available in cloud - using text input only
 audio_recorder_available = False
-from langchain.agents import create_sql_agent
-from langchain.sql_database import SQLDatabase
-from langchain.agents.agent_types import AgentType
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.agent_toolkits.sql.base import create_sql_agent
+from langchain_community.utilities import SQLDatabase
+from langchain.agents import AgentType
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from sqlalchemy import create_engine
 import sqlite3
 from langchain_openai import ChatOpenAI
